7823195da1ed7e02ba4a033a7dda8c0946626f0a
JONATHAN SENG YAW TONG <101211476@students.swinburne.edu.my>
2019-04-24 12:57:15 +0800

    Add class AddTaskMenu

--- gui.rb
+++ gui.rb
@@ -5,28 +5,7 @@ include Fox
 class Main < FXMainWindow
   def initialize(app)
     super(app, "_Routine", :width => 800, :height => 600)
-
-    # Main horizontal frame
-    hfrMain = FXHorizontalFrame.new(self, :opts => LAYOUT_FILL)
-
-    # Large vertical frame for title and pies
-    vfrListed = FXVerticalFrame.new(hfrMain, :opts => LAYOUT_FILL)
-    lbListedTitle = FXLabel.new(vfrListed, "Scheduled Tasks", :opts => LAYOUT_CENTER_X)
-
-    hfrListedTasks = FXVerticalFrame.new(vfrListed, :opts => LAYOUT_FILL)
-
-    # Skinny vertical frame for unlisted tasks and button
-    vfrUnlisted = FXVerticalFrame.new(hfrMain, :opts => LAYOUT_SIDE_RIGHT)
-    lbUnlistedTitle = FXLabel.new(vfrUnlisted, "Unscheduled Tasks", :opts => LAYOUT_CENTER_X)
-
-    vfrUnlistedTasks = FXVerticalFrame.new(vfrUnlisted, :opts => LAYOUT_FILL)
-
-    btAddTask = FXButton.new(vfrUnlisted, "+")
-
-    btAddTask.connect(SEL_COMMAND) do
-      task = generate_new_task
-    end
-
+		AddTaskMenu.new(self)
   end
 
   def create
@@ -41,7 +20,72 @@ end
 class Pie
 end
 
-class AddTask
+class DisplayTaskMenu < FXHorizontalFrame
+	def initialize(parent)
+		super(parent, :opts => LAYOUT_FILL)
+
+    # Large vertical frame for title and pies
+    vfrListed = FXVerticalFrame.new(self, :opts => LAYOUT_FILL)
+    lbListedTitle = FXLabel.new(vfrListed, "Scheduled Tasks", :opts => LAYOUT_CENTER_X)
+
+    hfrListedTasks = FXVerticalFrame.new(vfrListed, :opts => LAYOUT_FILL)
+
+    # Skinny vertical frame for unlisted tasks and button
+    vfrUnlisted = FXVerticalFrame.new(self, :opts => LAYOUT_SIDE_RIGHT)
+    lbUnlistedTitle = FXLabel.new(vfrUnlisted, "Unscheduled Tasks", :opts => LAYOUT_CENTER_X)
+
+    vfrUnlistedTasks = FXVerticalFrame.new(vfrUnlisted, :opts => LAYOUT_FILL)
+
+    btAddTask = FXButton.new(vfrUnlisted, "+")
+    btAddTask.connect(SEL_COMMAND) do
+      task = generate_new_task
+    end
+	end
+end
+
+class AddTaskMenu < FXVerticalFrame
+	def initialize(parent)
+		super(parent, :opts => LAYOUT_CENTER_X | LAYOUT_CENTER_Y)
+		
+		FXLabel.new(self, "Add New Task", :opts => LAYOUT_CENTER_X | LAYOUT_TOP)
+		
+		hfrInputZone = FXHorizontalFrame.new(self, :opts => LAYOUT_FILL)
+		
+		vfrLabels = FXVerticalFrame.new(hfrInputZone, :vSpacing => 8)
+		FXLabel.new(vfrLabels, "Title: ")
+		FXLabel.new(vfrLabels, "Description: ")
+		FXLabel.new(vfrLabels, "Start Time: ")
+		FXLabel.new(vfrLabels, "End Time: ")
+		
+		vfrInputs = FXVerticalFrame.new(hfrInputZone)
+		@inTaskTitle = FXTextField.new(vfrInputs, 50, :opts => FRAME_LINE)
+		@inTaskDesc = FXTextField.new(vfrInputs, 50, :opts => FRAME_LINE)
+		
+		hfrTaskStart = FXHorizontalFrame.new(vfrInputs)
+		@inTaskStartH = FXTextField.new(hfrTaskStart, 2, :opts => TEXTFIELD_INTEGER | TEXTFIELD_LIMITED | FRAME_LINE)
+		FXLabel.new(hfrTaskStart, ":")
+		@inTaskStartM = FXTextField.new(hfrTaskStart, 2, :opts => TEXTFIELD_INTEGER | TEXTFIELD_LIMITED | FRAME_LINE)
+		
+		hfrTaskEnd = FXHorizontalFrame.new(vfrInputs)
+		@inTaskEndH = FXTextField.new(hfrTaskEnd, 2, :opts => TEXTFIELD_INTEGER | TEXTFIELD_LIMITED | FRAME_LINE)
+		FXLabel.new(hfrTaskEnd, ":")
+		@inTaskEndM = FXTextField.new(hfrTaskEnd, 2, :opts => TEXTFIELD_INTEGER | TEXTFIELD_LIMITED | FRAME_LINE)
+		
+		hfrButtons = FXHorizontalFrame.new(self, :opts => LAYOUT_CENTER_X)
+		btAdd = FXButton.new(hfrButtons, "Add Task")
+		btAdd.connect(SEL_COMMAND) do 
+			valid = check_input
+		end
+		
+		btCancel = FXButton.new(hfrButtons, "Cancel")
+		btCancel.connect(SEL_COMMAND) do 
+			removeChild(self)
+		end
+	end
+	
+	def check_input 
+		
+	end
 end
 
 if __FILE__ == $0
